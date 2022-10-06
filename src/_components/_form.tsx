36704f40a0920d92a12510f1788cb161d97ef8e9
Eduardo Robles <robles@parati.ag>
2022-10-06 12:03:57 -0300

    Alteracao alerta do form

--- src/_components/_form.tsx
+++ src/_components/_form.tsx
@@ -171,15 +171,6 @@ function FormContact(props) {
 								Enviar
 							</Button>
 						</Form.Group>
-						<div className="alert alert-danger" role="alert">
-							<span>
-								<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" fill="currentColor" className="bi bi-exclamation-circle" viewBox="0 0 16 16">
-									<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-									<path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
-								</svg>
-							</span>
-							Não trabalhamos com licitações.
-						</div>
 						<p className="sou-color--white"></p>
 					</Form>
 				</Col>
